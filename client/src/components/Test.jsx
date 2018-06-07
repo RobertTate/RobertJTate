ed33e416691eea3a13f64a0edc7f4ec46b234547
Robert Tate <robertjosephtate@gmail.com>
2018-06-07 10:52:58 -0500

    testing app

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -1,10 +1,56 @@
 import React, { Component, Fragment } from 'react';
 
+import About from './About';
+import Languages from './Languages';
+import Projects from './Projects';
+import { SectionsContainer, Section } from 'react-fullpage';
 
-const Test = () => {
-    return (
-        <h1>fsfsdfsdf</h1>
-    )
+
+
+
+class App extends Component {
+
+    render() {
+
+        let options = {
+            sectionClassName: 'section',
+            anchors: ['Home', 'About', 'Languages', 'Projects'],
+            scrollBar: false,
+            navigation: true,
+            verticalAlign: false,
+            arrowNavigation: true
+        };
+
+        return (
+            <div>
+                <SectionsContainer className="container" {...options}>
+                    <Section className="custom-section" verticalAlign="true">
+                        <div id="slide1" className="hero-div slide">
+                            <div>
+                                <h1 className="fade-in1 oswald-white title">ROBERT TATE</h1>
+
+                                <h2 className="fade-in2">FULL STACK <br />DEVELOPMENT</h2>
+                            </div>
+                            <br />
+                        </div>
+                    </Section>
+                    {/* <Section>
+                        <About />
+                    </Section>
+                    <Section>
+                        <Languages />
+                    </Section>
+                    <Section>
+                        <Projects />
+                    </Section> */}
+                </SectionsContainer>
+            </div>
+
+
+        );
+    }
 }
 
-export default Test;
+
+
+export default App;
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -11,39 +11,9 @@ import { SectionsContainer, Section } from 'react-fullpage';
 class App extends Component {
 
     render() {
-
-        let options = {
-            sectionClassName: 'section',
-            anchors: ['Home', 'About', 'Languages', 'Projects'],
-            scrollBar: false,
-            navigation: true,
-            verticalAlign: false,
-            arrowNavigation: true
-        };
-
         return (
             <div>
-                <SectionsContainer className="container" {...options}>
-                    <Section className="custom-section" verticalAlign="true">
-                        <div id="slide1" className="hero-div slide">
-                            <div>
-                                <h1 className="fade-in1 oswald-white title">ROBERT TATE</h1>
-
-                                <h2 className="fade-in2">FULL STACK <br />DEVELOPMENT</h2>
-                            </div>
-                            <br />
-                        </div>
-                    </Section>
-                    {/* <Section>
-                        <About />
-                    </Section>
-                    <Section>
-                        <Languages />
-                    </Section>
-                    <Section>
-                        <Projects />
-                    </Section> */}
-                </SectionsContainer>
+                <h1>fdsfsdfds</h1>
             </div>
 
 
